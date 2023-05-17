5ce3ad73946a369a93e9801b438d10174d9946fa
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-05-18 02:59:03 +0530

    input-component-added

--- src/Components/ChatHeader.jsx
+++ src/Components/ChatHeader.jsx
@@ -1,17 +1,10 @@
 import { Flex, Box, Text, Image, Menu, MenuButton, MenuList, MenuItem, IconButton } from '@chakra-ui/react';
-import { HamburgerIcon, AddIcon, ExternalLinkIcon, RepeatIcon, EditIcon, PhoneIcon } from '@chakra-ui/icons';
+import { PhoneIcon } from '@chakra-ui/icons';
 import { GoKebabVertical, GoReport } from "react-icons/go"
 import { AiOutlineTeam } from "react-icons/ai"
-// To be changed dynamically later on
-const chatHeaderIcon = "https://source.unsplash.com/random?grayscale"
 
-// To be changed dynamically later on
-const from_head = "IGI Airport, T3"
-const to_head = "Sector 28"
-
-
-const ChatHeader = () => {
-  return (
+const ChatHeader = ({from_head, to_head, chatHeaderIcon}) => {
+  return <article>
     <Flex align="center" justify="space-between" px={2} gap={2}>
       <Flex align="center">
         <Image src={chatHeaderIcon} alt="Chat Header Icon" borderRadius="full" boxSize={12} mx={2} />
@@ -25,9 +18,9 @@ const ChatHeader = () => {
         </Box>
       </Flex>
 
-      <Menu width={"fit-content"} px={5}>
+      <Menu>
         <MenuButton as={IconButton} aria-label="Options" icon={<GoKebabVertical fontSize={20} />} background={"transparent"}  />
-        <MenuList width={"fit-content"}>
+        <MenuList width="100px" border={"none"}>
           <MenuItem icon={<AiOutlineTeam />}>
             Members
           </MenuItem>
@@ -40,7 +33,7 @@ const ChatHeader = () => {
         </MenuList>
       </Menu>
     </Flex>
-  );
+  </article>
 };
 
 export default ChatHeader;
--- src/Components/ChatScreen.jsx
+++ src/Components/ChatScreen.jsx
@@ -1,10 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChatHeader from './ChatHeader'
+import { Box, Input, IconButton, Menu, MenuButton, MenuList, MenuItem, Portal, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { AttachmentIcon } from '@chakra-ui/icons';
+import {AiOutlineSend, AiOutlineFileAdd, AiOutlineCamera, AiOutlineVideoCameraAdd} from "react-icons/ai"
+
+const MessageInput = () => {
+  const [message, setMessage] = useState('');
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSend = () => {
+    // Handle sending the message
+    console.log('Sending message:', message);
+    setMessage('');
+  };
+
+  const handleAttachFile = () => {
+    // Handle attaching a file
+    console.log('Attaching file...');
+  };
+
+  return (
+    <InputGroup 
+        width={"90vw"}
+        size='md'
+        margin={"auto"}
+        position="fixed"
+        bottom={10}
+        left={0}
+        right={0}
+        zIndex={9999}
+        display="flex"
+        alignItems="center"
+        bg={"white"}
+        boxShadow={"2xl"}
+    >
+      <Input
+        value={message}
+        onChange={handleChange}
+        placeholder="Reply to @Rohit Raj"
+        borderRadius="md"
+        mr={2}
+        border={"none"}
+        outline={"none"}
+      />
+      <InputRightElement width='5rem' pr={4}>
+        <Menu>
+            <MenuButton as={IconButton} aria-label="Attach file" icon={<AttachmentIcon  />} bg={"transparent"}/>
+            <MenuList minWidth="120px" bg={"#008000"} borderRadius={24} display={"flex"} justifyContent={"center"} alignItems={"center"} align={"center"}>
+                <MenuItem background={"transparent"} justifySelf={"center"} align={"center"} width={1/3} onClick={handleAttachFile}><AiOutlineCamera color='white' /></MenuItem>
+                <MenuItem background={"transparent"} justifySelf={"center"} align={"center"} width={1/3} onClick={handleAttachFile}><AiOutlineVideoCameraAdd  color='white'/></MenuItem>
+                <MenuItem background={"transparent"} justifySelf={"center"} align={"center"} width={1/3} onClick={handleAttachFile}><AiOutlineFileAdd  color='white'/></MenuItem>
+            </MenuList>
+        </Menu>
+
+        <AiOutlineSend fontSize={20} />
+      </InputRightElement>
+    </InputGroup>
+  );
+};
 
 const ChatScreen = () => {
-  return <article>
-    <ChatHeader />
-  </article>
+  return <main>
+    <ChatHeader 
+    // props to be later changed dynamically
+        chatHeaderIcon={"https://source.unsplash.com/random?grayscale"}
+        from_head={"IGI Airport, T3"}
+        to_head={"Sector 28"}
+    />
+
+    <MessageInput />
+  </main>
 }
 
 export default ChatScreen
