acd35de0f63fb0b33123750396950495ad89cadd
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-05-20 23:25:04 +0530

    pwa-implemented

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+       // add this to cache all the imports
+        workbox: {
+            globPatterns: ["**/*"],
+        },
+        // add this to cache all the
+        // static assets in the public folder
+        includeAssets: [
+            "**/*",
+        ],
+      manifest: {
+        name: "CHAT PWA -- CoRider",
+        short_name: "Chat PWA",
+        start_url: "./index.html",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type:"image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ]
+      },
+    })
+  ],
 })
