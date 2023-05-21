177cf6d6d4bb49255591f4895d56cfcdc5c7fecc
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-05-21 09:55:10 +0530

    pwa-fixes

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from "vite-plugin-pwa"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-       // add this to cache all the imports
-        workbox: {
-            globPatterns: ["**/*"],
-        },
-        // add this to cache all the
-        // static assets in the public folder
-        includeAssets: [
-            "**/*",
-        ],
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      // devOptions: {
+      //   enabled: false
+      // },
       manifest: {
-        name: "CHAT PWA -- CoRider",
-        short_name: "Chat PWA",
-        start_url: "./index.html",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#ffffff",
+        name: 'CHAT PWA -- CoRider',
+        short_name: 'Chat PWA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
         icons: [
           {
-            src: "/icon-192.png",
-            sizes: "192x192",
-            type:"image/png",
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "/icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
-        ]
+        ],
       },
-    })
+    }),
   ],
 })
